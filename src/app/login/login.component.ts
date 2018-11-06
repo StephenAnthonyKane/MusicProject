67b3ac1322a40b40254ba97ca83bab6dff362479
Evan2804 <evan.metcalfe2804@gmail.com>
2018-11-06 00:05:51 +0000

    Merge conflict fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,19 +7,11 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
-<<<<<<< HEAD
-  private response_type: string = "token";
-  private clientId: string = "10ce89cd71244a4b866a20bcf768a81e";
-  private redirect_uri: string = "http://localhost:4200/callback";
-  private scope: string = "user-read-private user-read-email";
-  private show_dialog: string = "true"
-=======
   private response_type = 'token';
   private clientId = '10ce89cd71244a4b866a20bcf768a81e';
   private redirect_uri = 'http://localhost:4200/callback';
   private scope = 'user-read-private user-read-email';
   private show_dialog = 'false';
->>>>>>> 5f4ec62... tslint fixes.
 
   spotifyLogin() {
     window.location.href = 'https://accounts.spotify.com/authorize?response_type=' + this.response_type +
