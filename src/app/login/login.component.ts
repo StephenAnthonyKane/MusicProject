74da91f61a8d9d3bd91e6924a68922cf2395f18e
Evan2804 <evan.metcalfe2804@gmail.com>
2018-11-12 22:50:35 +0000

    Styling changes.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,11 +9,12 @@ export class LoginComponent {
 
   private response_type = 'token';
   private clientId = '10ce89cd71244a4b866a20bcf768a81e';
-  private redirect_uri = 'http://localhost:4200/callback';
+  // private redirect_uri = 'http://localhost:4200/callback';
+  private redirect_uri = 'http://192.168.0.16:4200/callback';
   private scope = 'user-read-private user-read-email playlist-read-private ' +
                   'user-read-currently-playing playlist-read-collaborative ' +
                   'playlist-modify-private playlist-read-private playlist-modify-public';
-  private show_dialog = 'false';
+  private show_dialog = 'true';
 
   spotifyLogin() {
     window.location.href = 'https://accounts.spotify.com/authorize?response_type=' + this.response_type +
